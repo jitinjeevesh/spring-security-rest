aa321941a874c1fa01ffa2f9762d7394b91a1f7c
Jeevesh Pandey <jpandey@dminc.com>
2016-09-23 17:08:30 +0530

    Modification:Added custom token generation method

--- Spring-security-REST/src/main/java/com/oauth/authentication/generator/TokenGenerator.java
+++ Spring-security-REST/src/main/java/com/oauth/authentication/generator/TokenGenerator.java
@@ -2,7 +2,15 @@ package com.oauth.authentication.generator;
 
 import com.oauth.data.AuthenticationToken;
 
+/**
+ * This is used to generate token strategy.
+ */
 public interface TokenGenerator {
 
+    /**
+     * This method is used to generate the authentication token. Override this method if you want your own generation strategy.
+     *
+     * @return AuthenticationToken with token, exp time.
+     */
     AuthenticationToken generate();
 }
--- Spring-security-REST/src/main/java/com/oauth/service/RESTSpringSecurityService.java
+++ Spring-security-REST/src/main/java/com/oauth/service/RESTSpringSecurityService.java
@@ -1,6 +1,7 @@
 package com.oauth.service;
 
 import com.oauth.authentication.config.TokenGeneratorConfig;
+import com.oauth.authentication.generator.TokenGenerator;
 import com.oauth.authentication.generator.TokenGeneratorFactory;
 import com.oauth.data.AuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,23 @@ public class RESTSpringSecurityService {
     @Autowired
     private TokenGeneratorFactory tokenGeneratorFactory;
 
+    /**
+     * This method is used to generate token with the pre defined strategies for token generation.
+     * For eg: JWT, UUID
+     *
+     * @return AuthenticationToken with all the token information.
+     */
     public AuthenticationToken generateToken() {
         return tokenGeneratorFactory.generate(tokenGeneratorConfig.getAuthentication());
+    }
+
+    /**
+     * This method is used to generate token with the user defined strategies.
+     *
+     * @return AuthenticationToken with all the token information.
+     */
+    public AuthenticationToken generateToken(TokenGenerator tokenGenerator) {
+        return tokenGenerator.generate();
     }
 
 }
