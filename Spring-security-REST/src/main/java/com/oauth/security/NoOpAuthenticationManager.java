563634e0ae760d1d62f8d551861d524533ccf899
Jeevesh Pandey <jpandey@dminc.com>
2016-09-28 12:42:29 +0530

    Modification:Declare NoOpAuthenticationManager as @Component.

--- Spring-security-REST/src/main/java/com/oauth/security/NoOpAuthenticationManager.java
+++ Spring-security-REST/src/main/java/com/oauth/security/NoOpAuthenticationManager.java
@@ -3,7 +3,9 @@ package com.oauth.security;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
 
+@Component
 public class NoOpAuthenticationManager implements AuthenticationManager {
 
     @Override
